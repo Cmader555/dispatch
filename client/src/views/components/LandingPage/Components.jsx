b7d1d40a7824aca3d727ce7bb8acc82d34b53294
Christian Mader <Cmader555@users.noreply.github.com>
2019-08-16 22:54:11 -0700

    begining setting up userAuth

---
+++ client/src/views/components/LandingPage/Components.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Input from '@material-ui/core/Input';
+
+
+class landingPage extends React.Component {
+
+    state = {
+        username: "",
+        password: ""
+    }
+
+    render() {
+
+        <div>
+            <div className="logIn">
+
+            </div>
+
+        </div>
+    }
+
+
+}
