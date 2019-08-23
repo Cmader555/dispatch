d8cfb71e500001d6d60313f06968be611db34cbd
Christian Mader <Cmader555@users.noreply.github.com>
2019-08-23 11:28:20 -0700

    set up configuration files

---
+++ config/middleware/passport.js
@@ -0,0 +1,53 @@
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+const keys = require("../../config/keys");
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+
+var db = require("../../models");
+
+// Telling passport we want to use a Local Strategy. In other words, we want login with a username and password
+passport.use(new LocalStrategy( 
+  // Our user will sign in using a username, rather than a "email"
+  {
+    usernameField: "username"
+  },
+  function (username, password, done,) {
+    // When a user tries to sign in this code runs
+    db.User.findOne({ username }).then(function (dbUser) {
+      // If there's no user with the given email
+      if (!dbUser) {
+        return done(null, false, {
+          message: "Incorrect username"
+        });
+      }
+      // If there is a user with the given username, but the password the user gives us is incorrect
+      else if (!dbUser.validPassword(password)) {
+        return done(null, false, {
+          message: "Incorrect password."
+        });
+      }
+      // If none of the above, return the user
+      return done(null, dbUser);
+    });
+  }
+));
+
+passport.use(
+  new JwtStrategy(opts, (jwt_payload, done) => {
+    User.findById(jwt_payload.sub)
+      .then(user => {
+        if (user) {
+          return done(null, user);
+        }
+        return done(null, false);
+      })
+      .catch(err => console.log(err));
+  })
+);
