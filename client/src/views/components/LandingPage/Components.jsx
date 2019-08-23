4d0597253a4f7bff68a0d13d6c98d017de0e9f3e
Christian Mader <Cmader555@users.noreply.github.com>
2019-08-23 15:59:22 -0700

    log in functionality, added functions

--- client/src/views/components/LandingPage/Components.jsx
+++ client/src/views/components/LandingPage/Components.jsx
@@ -2,31 +2,76 @@ import React from "react";
 import Input from '@material-ui/core/Input';
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
-
+import swal from 'sweetalert';
+import API from '../../../utils/API'
 
 
 class LandingPage extends React.Component {
 
     state = {
-        username: "",
+        userID: "",
         password: ""
     }
+
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    onSubmit = (event) => {
+        event.preventDefault();
     
+        const user = {
+          userID: this.state.userID,
+          password: this.state.password
+        };
+    
+        API.userLogin(user)
+          .then(({ data }) => {
+            //console.log("yo", data.user._id)
+            // this.setState({
+            //   _id: data.user._id,
+            //   token: data.token
+            // })
+            swal("log in was successful")
+    
+          })
+          .catch((err) => {
+            console.log(err)
+            swal("Oops", "You entered the wrong password or username!", "error")
+          });
+    
+      };
 
     render() {
         const logIn = {
-           display: "block"
-          };
+            display: "block"
+        };
         return (
             <div>
                 <div>
                     <FormControl style={logIn}>
-                        <InputLabel htmlFor="my-input">Username</InputLabel>
-                        <Input id="my-input" aria-describedby="my-helper-text" />
+                        <InputLabel htmlFor="userID">User ID</InputLabel>
+                        <Input
+                            value={this.state.userID}
+                            onChange={this.handleInputChange}
+                            id="userID"
+                            aria-describedby="my-helper-text"
+                            name="userID"
+                        />
                     </FormControl>
                     <FormControl style={logIn}>
-                        <InputLabel htmlFor="my-input">Password</InputLabel>
-                        <Input id="my-input" aria-describedby="my-helper-text" />
+                        <InputLabel htmlFor="password">Password</InputLabel>
+                        <Input
+                            value={this.state.password}
+                            onChange={this.handleInputChange}
+                            id="password"
+                            aria-describedby="my-helper-text"
+                            name="password"
+                        />
                     </FormControl>
                 </div>
 
